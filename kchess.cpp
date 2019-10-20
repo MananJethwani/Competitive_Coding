694650e818db2f38b59c2f5025177e8d96ecf64f
Siddharth-rs <56801217+Siddharth-rs@users.noreply.github.com>
2019-10-21 00:50:03 +0530

    Create kchess.cpp
    All submissions for this problem are available.Abhineet the Chess master of NIT Kurukshetra got bored of 8x8 chess board and invented the new variation of Chess, the one on an infinite chess board. There is only a white king and N black knights. The white king has to avoid checkmate as long as it can.
    
    A situation is given. Determine if white king is in checkmate or not. The white king is in checkmate if and only if it is in check and it is not able to move to any of its neighboring positions which is not in check

---
+++ kchess.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+typedef unsigned long long int ull;
+typedef long long int ll;
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        int x[N],y[N];
+        for(int i=0;i<N;i++)
+            cin>>x[i]>>y[i];
+        int A,B;
+        cin>>A>>B;
+        bool ans=true;
+        for(int i=-1;i<=1;i++)
+        {
+            for(int j=-1;j<=1;j++)
+            {
+                if(i==0 && j==0)
+                    continue;
+                int a=A+i,b=B+j;
+                bool check=false;
+                for(int i=0;i<N;i++)
+                    if((abs(a-x[i])==2 && abs(b-y[i])==1) || (abs(a-x[i])==1 && abs(b-y[i])==2))
+                    {
+                        check=true;
+                        break;
+                    }
+                if(check==false)
+                    ans=false;
+        }
+    }
+        if(ans)
+            cout<<"YES"<<endl;
+        else
+            cout<<"NO"<<endl;
+    }
+    return 0;
+}
